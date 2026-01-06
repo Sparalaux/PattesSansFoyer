5c80c612a7e4cfeb15d7457376ac5e47efae52d4
Sparalaux <theodeletang@gmail.com>
2026-01-06 11:15:34 +0100

    changement de css mineure

--- src/Controller/AnimauxController.php
+++ src/Controller/AnimauxController.php
@@ -12,7 +12,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class AnimauxController extends AbstractController
 {
